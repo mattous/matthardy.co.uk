e2e2fb35a8004ab0dcbe83136bae9f5a4679b944
mattous <mattous@mattous.tk>
2024-06-27 21:59:29 +0100

    fixed s3 bucket endpoint

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "cname" {
   zone_id = var.zone_id
   name    = var.website_address
-  value   = aws_s3_bucket.bucket.bucket_domain_name
+  value   = aws_s3_bucket.bucket.website_domain
   type    = "CNAME"
   proxied = true
 }
